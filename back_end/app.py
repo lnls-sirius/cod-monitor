648310b4309e2bd9758c821edd6fc9532de75312
RafaelLyra8 <rafaellyra2010@hotmail.com>
2023-02-13 20:27:15 -0300

    Containerization (Frontend and Backend)

--- back_end/app.py
+++ back_end/app.py
@@ -73,4 +73,4 @@ def home():
 
 if __name__ == "__main__":
     app.SIGNATURES = {}
-    app.run(host="127.0.0.1", port=8080, debug=True)
+    app.run()
--- build.sh
+++ build.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
 
-version="1.0.0"
-
-cd ./front_end
 docker-compose -f docker-compose.yml up
-
-docker tag orb-mon-frontend:latest orb-mon-frontend:$version
-# docker build --build-arg version=$version --tag orb-mon-frontend:$version .
-
-# cd ../back_end
-# docker build --build-arg version=$version --tag orb-mon-backend:$version .
