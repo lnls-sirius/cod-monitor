1f58a92f47b067d912ee0869fa54de0a36f6fd9f
Rafael Lyra <rafael.lyra@lnls.br>
2023-04-18 14:26:17 -0300

    Git info comment

--- front_end/src/components/Structure/Footer/index.tsx
+++ front_end/src/components/Structure/Footer/index.tsx
@@ -1,27 +1,27 @@
 import React from "react";
-import GitInfo, { GitInformation } from 'react-git-info/macro';
+// import GitInfo, { GitInformation } from 'react-git-info/macro';
 import * as S from './styled';
 
 const Menu: React.FC = (): React.ReactElement => {
   // Display the footer of the page
   function get_version(): string {
-    let git_version: GitInformation;
-    try{
-      git_version = GitInfo();
-    }catch{
-      git_version = {
-        tags: [],
-        commit: {
-          hash: '',
-          shortHash: '',
-          date: '',
-          message: ''
-        }
-      };
-    }
-    return "v1.0.0."+
-      git_version.commit.shortHash + " / " +
-      git_version.commit.date;
+    // let git_version: GitInformation;
+    // try{
+    //   git_version = GitInfo();
+    // }catch{
+    //   git_version = {
+    //     tags: [],
+    //     commit: {
+    //       hash: '',
+    //       shortHash: '',
+    //       date: '',
+    //       message: ''
+    //     }
+    //   };
+    // }
+    return "v1.0.0.";
+      // + git_version.commit.shortHash + " / " +
+      // git_version.commit.date;
   }
 
   return (
