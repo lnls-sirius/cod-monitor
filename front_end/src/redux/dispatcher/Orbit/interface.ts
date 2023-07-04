8a96c2f0242ad5b277cb735c237afe82c28820e4
Rafael Lyra <rafael.lyra@lnls.br>
2023-07-04 10:28:56 -0300

    Add Cod rebuilt changed flag

--- front_end/src/redux/dispatcher/Orbit/interface.ts
+++ front_end/src/redux/dispatcher/Orbit/interface.ts
@@ -2,5 +2,6 @@ import { BaseStrArrayDict } from "../../../assets/interfaces/patterns";
 
 export default interface OrbitDispatcher{
     setChangeOrbit(change: boolean): void;
+    setChangeCodOrbit(change: boolean): void;
     setSignatureListInterface(list: BaseStrArrayDict): void;
 }
